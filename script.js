82e7ba8a710dceb877c707e75be9e18a2d96b050
Luc <luchio83@hotmail.fr>
2022-07-10 15:05:54 +0200

    add the first level

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 const firstClick = document.querySelector('#testClick');
 const secondClick = document.querySelector('#testClick2');
+const thirdClick = document.querySelector('#testClick3');
+const fourthClick = document.querySelector('#testClick4');
+const fiveClick = document.querySelector('#testClick5');
 const menuToDisplay = document.querySelector('#ulToDisplay');
-const menuToDisplayS = document.querySelector('#ulToDisplay2');
+const menuToDisplay2 = document.querySelector('#ulToDisplay2');
+const menuToDisplay3 = document.querySelector('#ulToDisplay3');
+const menuToDisplay4 = document.querySelector('#ulToDisplay4');
+const menuToDisplay5 = document.querySelector('#ulToDisplay5');
 
 firstClick.addEventListener('click', function() {
     if (menuToDisplay.classList.contains('display-none')) {
     menuToDisplay.classList.remove('display-none');
-    menuToDisplayS.classList.add('display-none');
-    } else {
-    menuToDisplay.classList.add('display-none');
-    }
+    menuToDisplay2.classList.add('display-none');
+    menuToDisplay3.classList.add('display-none');
+    menuToDisplay4.classList.add('display-none');
+    menuToDisplay5.classList.add('display-none');
+    } 
 });
 
 secondClick.addEventListener('click', function() {
     
-    if (menuToDisplayS.classList.contains('display-none')) {
-    menuToDisplayS.classList.remove('display-none');
+    if (menuToDisplay2.classList.contains('display-none')) {
+    menuToDisplay2.classList.remove('display-none');
     menuToDisplay.classList.add('display-none');
-    } else {
-    menuToDisplayS.classList.add('display-none');
-    }
+    menuToDisplay3.classList.add('display-none');
+    menuToDisplay4.classList.add('display-none');
+    menuToDisplay5.classList.add('display-none');
+    } 
 });
+
+thirdClick.addEventListener('click', function() {
+    if (menuToDisplay3.classList.contains('display-none')) {
+    menuToDisplay3.classList.remove('display-none');
+    menuToDisplay.classList.add('display-none');
+    menuToDisplay2.classList.add('display-none');
+    menuToDisplay4.classList.add('display-none');
+    menuToDisplay5.classList.add('display-none');
+    } 
+});
+
+fourthClick.addEventListener('click', function() {
+    if (menuToDisplay4.classList.contains('display-none')) {
+    menuToDisplay4.classList.remove('display-none');
+    menuToDisplay.classList.add('display-none');
+    menuToDisplay2.classList.add('display-none');
+    menuToDisplay3.classList.add('display-none');
+    menuToDisplay5.classList.add('display-none');
+    } 
+});  
+
+fiveClick.addEventListener('click', function() {
+    if (menuToDisplay5.classList.contains('display-none')) {
+    menuToDisplay5.classList.remove('display-none');
+    menuToDisplay.classList.add('display-none');
+    menuToDisplay2.classList.add('display-none');
+    menuToDisplay3.classList.add('display-none');
+    menuToDisplay4.classList.add('display-none');
+    } 
+});
+
 
 
 const sectionClickable = document.querySelector('#sectionTest');
@@ -30,6 +69,9 @@ sectionClickable.addEventListener('click', function(e) {
         
     }else {
         menuToDisplay.classList.add('display-none');
-        menuToDisplayS.classList.add('display-none');
+        menuToDisplay2.classList.add('display-none');
+        menuToDisplay3.classList.add('display-none');
+        menuToDisplay4.classList.add('display-none');
+        menuToDisplay5.classList.add('display-none');
     }
 });
